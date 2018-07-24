f7a313cdfd9c982c31fc45d053565fe5caa13a77
hongge <1361255790.@qq.com>
2018-07-25 00:48:11 +0800

    评论框的弹出

--- src/main/webapp/js/sinamain.js
+++ src/main/webapp/js/sinamain.js
@@ -4,5 +4,16 @@ window.onload=function(){
 			alert(1)
 		$("#weiboForm").submit()
       })
+      var open=false;
+      $(".show").on("click",".fun .pinglun",event,function(){
+    	  if(open){
+    		  $(this).parent().parent().find(".comment").css({"display":"none","opacity":"0"})
+    		  open=false;
+    		  return ;
+    	  }
+		$(this).parent().parent().find(".comment").css({"display":"block","opacity":"1"})
+		open=true;
+	})
+      
 
 }
